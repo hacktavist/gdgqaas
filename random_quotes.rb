ba4a6bf92da5cd3596a784e69de00afa821ddcbc
hacktavist <nicwithoutak@Nics-MacBook-Pro.local>
2014-01-17 18:06:00 -0500

    Just added a few more quotes

--- random_quotes.rb
+++ random_quotes.rb
@@ -6,9 +6,13 @@ require 'shotgun'
 ###########################
 
 def quote_gen(name)
-  nic_quote_array = ["FUCKING LOGIC!!!"];
+  nic_quote_array = ["FUCKING LOGIC!!!", "How do you feel about assisted suicide...cuz that is what you are doing to me!"];
   ben_quote_array = ["I want to see your console"];
   rick_quote_array = ["Mo' money", "Blah blah"];
+  mcg_quote_array = ["GFY", "Something about killing somebody"];
+  brandon_quote_array = ["Sucker!", "High five!"];
+  keith_quote_array = ["Is the upgrade done yet?", "They are paying us more money!"];
+  mike_quote_array = ["Can I kill them?", "FUCK THAT!", "Hell no"];
   name = name.downcase;
 
 
@@ -18,6 +22,14 @@ def quote_gen(name)
     quote = sample ben_quote_array.sample;
   elsif (name == "rick")
     quote = rick_quote_array.sample;
+  elsif (name == "mcgraw")
+    quote = mcg_quote_array.sample;
+  elsif (name == "brandon")
+    quote = brandon_quote_array.sample;
+  elsif (name == "keith")
+    quote = keith_quote_array.sample;
+  elsif (name == "mike")
+    quote = mike_quote_array.sample;
   else
     quote = "That beeotch don't work here";
   end
